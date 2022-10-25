2c53353a87ba2b1e8b5234f8732774c346aafb04
Adrian Aviles <adrian8167e@gmail.com>
2022-10-24 18:31:46 -0600

    Reset stream position
    If the stream supports seeking, after reading all bytes, the stream is reset to its original position

--- Flowsy.Core/StreamExtensions.cs
+++ Flowsy.Core/StreamExtensions.cs
@@ -6,13 +6,21 @@ public static class StreamExtensions
     {
         if (stream.Length == 0)
             return Array.Empty<byte>();
+
+        var position = stream.Position;
+
+        if (stream.CanSeek)
+            stream.Seek(0, SeekOrigin.Begin);
         
         var bytes = new byte[stream.Length];
         int b;
         long i = 0;
 
         while ((b = stream.ReadByte()) >= 0)
-            bytes[i++] = (byte)b;
+            bytes[i++] = (byte) b;
+        
+        if (stream.CanSeek)
+            stream.Seek(position, SeekOrigin.Begin);
         
         return bytes;
     }
