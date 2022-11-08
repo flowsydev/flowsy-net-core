cf1920808c2a5f48e9d670fb98f9e47627bc9cb8
Adrian Aviles <adrian8167e@gmail.com>
2022-11-07 22:13:20 -0700

    Fix stream reading
    Improve stream reading when converting to array.

--- Flowsy.Core/StreamExtensions.cs
+++ Flowsy.Core/StreamExtensions.cs
@@ -1,29 +1,28 @@
+using System.Buffers;
+
 namespace Flowsy.Core;
 
 public static class StreamExtensions
 {
     public static byte[] ToArray(this Stream stream)
     {
-        if (stream.Length == 0)
-            return Array.Empty<byte>();
-
         long? position = null;
         if (stream.CanSeek)
         {
             position = stream.Position;
             stream.Seek(0, SeekOrigin.Begin);
         }
-        
-        var bytes = new byte[stream.Length];
-        int b;
-        long i = 0;
 
+        var memoryOwner = MemoryPool<byte>.Shared.Rent();
+        var memory = memoryOwner.Memory;
+
+        int b, i = 0;
         while ((b = stream.ReadByte()) >= 0)
-            bytes[i++] = (byte) b;
+            memory.Span[i++] = (byte) b; 
         
         if (position is not null)
             stream.Seek(position.Value, SeekOrigin.Begin);
         
-        return bytes;
+        return memory.ToArray();
     }
 }
